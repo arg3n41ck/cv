18713ca54a76f351e37097fbca317f49b3a2954a
Argen <argenalimbaev@gmail.com>
2024-03-02 17:59:37 +0600

    fix not saving theme value

--- index.js
+++ index.js
@@ -16,7 +16,7 @@ const updateThemeStyles = theme => {
 
 const toggleTheme = () => {
 	const currentTheme = document.body.getAttribute('data-theme')
-	const newTheme = currentTheme === 'dark' ? '' : 'dark'
+	const newTheme = currentTheme === 'dark' ? 'light' : 'dark'
 	document.body.setAttribute('data-theme', newTheme)
 	localStorage.setItem('theme', newTheme)
 	updateThemeStyles(newTheme)
